37836c85ac3a6326cc2b5e6b32509707eb0ca187
jojosen <jojo.33493@gmail.com>
2022-06-30 16:53:12 +0200

    Initial Commit

---
+++ Groupic_WebView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Groupic_WebView
+//
+//  Created by Johannes Busch on 30.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Groupic_WebView/Groupic_WebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Groupic_WebViewApp.swift
+//  Groupic_WebView
+//
+//  Created by Johannes Busch on 30.06.22.
+//
+
+import SwiftUI
+
+@main
+struct Groupic_WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
