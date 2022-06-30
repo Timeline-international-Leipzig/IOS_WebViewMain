01e5efc30600d1460695ab05ae5df8dcd1d80e65
anatolijtravkin <anatolijtrav@gmail.com>
2022-06-30 18:14:38 +0200

    Update: Version 0.0.1

--- Groupic_WebView/Extensions.swift
+++ Groupic_WebView/Extensions.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 import WebKit
 
+/*
 extension WKWebView {
     override open var safeAreaInsets: UIEdgeInsets {
         return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
 }
+*/
